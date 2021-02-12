5b739bf67f58d492d247103cb90cd18f8bc04c6c
david_llanes <david.llanes@nextia.mx>
2021-02-12 17:08:48 -0600

    added change password test

--- arion movil/ui/profile/change pass email/ChangePasswordEmail.swift
+++ arion movil/ui/profile/change pass email/ChangePasswordEmail.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct ChangePasswordEmail: View {
     @StateObject var viewModel = ProfileViewModel()
     @State var newEmail:String = ""
+    @State var newPassword:String = ""
+    @State var verifyPassword:String = ""
+    @State var currentPassword:String = ""
     var body: some View {
         ZStack{
             Color("background")
@@ -19,17 +22,17 @@ struct ChangePasswordEmail: View {
                     Section(header: TextWithCustomFonts("Cambiar correo",customFont: CustomFont(type: .bold, size: 20))){
                         VStack{
                             TextWithCustomFonts("Modificar el correo de tu cuenta guardando todas tus configuraciones",customFont: CustomFont(type: .light, size: 16)).frame(height:60)
-                            CustomTextField(textValue: $newEmail, title: "Escribe tu email").keyboardType(.numberPad)
+                            CustomTextField(textValue: $newEmail, title: "Escribe tu email")
                         }
                     }
                     Section(header: TextWithCustomFonts("Cambiar contraseña",customFont: CustomFont(type: .bold, size: 20))){
-                        CustomTextField(textValue: $newEmail, title: "Escribe tu contraseña actual").keyboardType(.numberPad)
-                        CustomTextField(textValue: $newEmail, title: "Escribe nueva contraseña").keyboardType(.numberPad)
-                        CustomTextField(textValue: $newEmail, title: "Confirma tu contraseña").keyboardType(.numberPad)
+                        CustomTextField(textValue: $currentPassword, title: "Escribe tu contraseña actual")
+                        CustomTextField(textValue: $newPassword, title: "Escribe nueva contraseña")
+                        CustomTextField(textValue: $verifyPassword, title: "Confirma tu contraseña")
                     }
                 }
                 RectangleBtn("Guardar"){
-                    viewModel.changeProfile(body: ChangeProfileBody(options: "ChangeUserPassword", newEmail: "", currentPassword: "1234567", newPassword: "123456"))
+                    viewModel.changeProfile(body: ChangeProfileBody(options: "ChangeUserPassword", newEmail: "", currentPassword: currentPassword, newPassword: newPassword))
                     
                 }.frame(width:200,height:40)
                     .frame(minWidth:0, maxWidth: .infinity, alignment: .center).padding()
